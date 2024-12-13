eb93e72381399c97820d93fcd342daeb3bb488d8
KienNgo <23520799@gm.uit.edu.vn>
2024-12-13 10:49:08 +0700

    update

--- Lab5/SendGmail.cs
+++ Lab5/SendGmail.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
+using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace Lab5
 {
     public partial class SendGmail : Form
     {
+        private List<Attachment> attachments = new List<Attachment>(); // Danh sách file đính kèm
+
         public SendGmail()
         {
             InitializeComponent();
@@ -31,6 +26,13 @@ namespace Lab5
             mail.To.Add(toGmail);
             mail.Subject = subject;
             mail.Body = richTextBox_Body.Text;
+            mail.IsBodyHtml = true; // Hỗ trợ HTML trong email
+
+            // Thêm file đính kèm từ danh sách
+            foreach (var attachment in attachments)
+            {
+                mail.Attachments.Add(attachment);
+            }
 
             SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
             {
@@ -47,6 +49,35 @@ namespace Lab5
             catch (Exception ex)
             {
                 MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button_attachFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Multiselect = true; // Cho phép chọn nhiều file
+                openFileDialog.Filter = "All Files (*.*)|*.*"; // Chấp nhận mọi loại file
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        foreach (string filePath in openFileDialog.FileNames)
+                        {
+                            // Thêm file đính kèm vào danh sách
+                            Attachment attachment = new Attachment(filePath);
+                            attachments.Add(attachment);
+                            // Hiển thị thông tin file đã chọn vào RichTextBox
+                            richTextBox_Body.AppendText($"{filePath}\n");
+                        }
+                        MessageBox.Show("Files attached successfully!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error adding attachment: " + ex.Message);
+                    }
+                }
             }
         }
     }
